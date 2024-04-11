dfcbe394375567362003824ec15ba77ffc13cc58
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-12 06:58:38 +1200

    changed title

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,7 @@ function App() {
         </a>
       </div>
       <h1>ORIMI</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
