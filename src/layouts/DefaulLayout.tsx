d3d58234c37fe92a9cd4a593bc3d9e37185f128e
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-16 11:53:49 +1200

    feat: add Footer

--- src/layouts/DefaulLayout.tsx
+++ src/layouts/DefaulLayout.tsx
@@ -1,13 +1,15 @@
+import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import { Outlet } from 'react-router-dom'
 
-
 export const DefaultLayout = () => {
   return (
-    <main>
-    <Navbar/>
-
-      <Outlet />
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
