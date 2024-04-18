24e91a1820fcf1d24f37af26e7e7bada6d573273
Pedro Vega <pedro.vega.damian@gmail.com>
2024-04-18 18:38:31 -0500

    feat: add Navbar and Footer components

--- src/layouts/DefaulLayout.tsx
+++ src/layouts/DefaulLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
 
 export const DefaultLayout = () => {
   return (
-    <main>
+    <main className="bg-bg_color">
+      <Navbar />
       <Outlet />
+      <Footer />
     </main>
   )
 }
