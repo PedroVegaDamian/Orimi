dba9557e310394ac06213c64096e2ec4be681aa6
Pedro Vega <pedro.vega.damian@gmail.com>
2024-04-18 18:38:31 -0500

    feat: add Navbar and Footer components

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,13 @@
-import { HomePage } from '@/pages/Home'
-import { AboutPage } from '@/pages/About'
-import { DefaultLayout } from '../layouts/DefaulLayout'
 import { Navigate, createBrowserRouter } from 'react-router-dom'
+
+import { DefaultLayout } from '@/layouts/DefaulLayout'
+import { HomePage } from '@/pages/Home'
+import { CartPage } from '@/pages/Cart'
+import { AboutPage } from '@/pages/About'
+import { LoginPage } from '@/pages/Login'
+import { ContactPage } from '@/pages/Contact'
+import { ProductsPage } from '@/pages/Products'
+import { FavoritesPage } from '@/pages/Favorites'
 
 export const router = createBrowserRouter([
   {
@@ -13,8 +19,28 @@ export const router = createBrowserRouter([
         element: <HomePage />
       },
       {
+        path: 'products',
+        element: <ProductsPage />
+      },
+      {
         path: 'about',
         element: <AboutPage />
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'login',
+        element: <LoginPage />
+      },
+      {
+        path: 'favorites',
+        element: <FavoritesPage />
+      },
+      {
+        path: 'cart',
+        element: <CartPage />
       }
     ]
   },
