011b320e6327874e351ead11197e2e117b16fe5e
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-19 13:10:22 +1200

    feat: detructuring the user object

--- src/services/login.ts
+++ src/services/login.ts
@@ -4,8 +4,7 @@ export const signIn = async (auth: Auth, email: string, password: string) => {
   try {
     const { user } = await signInWithEmailAndPassword(auth, email, password)
     return { success: true, user }
-    }
-  } catch (error: unknown) {
+    } catch (error: unknown) {
     if (error instanceof Error) {
       return { success: false, message: error.message }
     }
