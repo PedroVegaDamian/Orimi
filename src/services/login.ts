4b33b3ad61aec26eaef4a4f678bc79413bb025b6
Rocio Peralta <108355326+rocio-peralta@users.noreply.github.com>
2024-04-19 12:59:34 +1200

    Update src/services/login.ts
    Co-authored-by: Pedro Vega Damian <60680972+PedroVegaDamian@users.noreply.github.com>

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,6 +1,6 @@
 import { signInWithEmailAndPassword, Auth } from 'firebase/auth'
 
-export const signInService = async (auth: Auth, email: string, password: string) => {
+export const signIn = async (auth: Auth, email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password)
     if (auth.currentUser) {
