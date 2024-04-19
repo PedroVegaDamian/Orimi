8a13f4a128c4528ac880e8979a14cfe64ff4e57a
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-19 16:33:08 +1200

    feat: working on styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,31 +15,31 @@ export const LoginPage = () => {
 
   return (
     <>
-      <section className="bg-gray-50">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
-          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+      <section className="">
+        <div className="flex flex-col items-center justify-center px-6 py-8">
+          <div className="w-full max-w-md ">
             <div className="p-6 space-y-4 ">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Sign in to your account
+              <h1 className="font-nunito text-22 font-bold text-center text-primary_800_color text-2xl">
+                Sign in
               </h1>
-              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900">
+              <form onSubmit={handleSubmit} className="space-y-6">
+                <div className="mt-10">
+                  <label className="block font-nunito text-sm font-semibold leading-19 text-left text-black_color mb-2">
                     Your email
                   </label>
                   <input
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    className="border border-solid border-grey_color text-gray-900 rounded-lg focus:ring-red-500 focus:border-grey_800_color block w-full p-2.5"
                     placeholder="name@company.com"
                     onChange={e => setEmail(e.target.value)}
                   ></input>
-                  <a className="text-red-600 text-sm font-medium">
+                  <a className="text-red_color text-sm font-medium">
                     {errorEmail ? <p>{errorEmail}</p> : null}
                   </a>
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900">
+                  <label className="block font-nunito text-sm font-semibold leading-19 text-left text-black_color mb-2">
                     Password
                   </label>
                   <input
@@ -47,38 +47,42 @@ export const LoginPage = () => {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    className="border border-solid border-grey_color text-gray-900 rounded-lg focus:ring-red-500 focus:border-grey_800_color block w-full p-2.5"
                     onChange={e => setPassword(e.target.value)}
                   ></input>
-                  <a className="text-red-600 text-sm font-medium">
+                  <a className="text-red_color text-sm font-medium">
                     {errorPassword ? <p>{errorPassword}</p> : null}
                   </a>
+                  <div className="flex justify-center">
+                    <a className="text-red_color text-sm font-medium pt-5">
+                      {error ? <p>{error}</p> : null}
+                    </a>
+                  </div>
                 </div>
-                <div className="flex items-center"></div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-purple-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
-                  Sign in
-                </button>
-                <a className="text-red-600 text-sm font-medium">
-                  {error ? <p>{error}</p> : null}
-                </a>
-                <a
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 justify-center"
-                  onClick={() => navigate('/reset-password')}
-                >
-                  Forgot password?
-                </a>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{' '}
-                  <a
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    onClick={() => navigate('/register')}
+
+                <div className="flex flex-col items-center justify-center p">
+                  <button
+                    type="submit"
+                    className="block w-32 bg-primary_color font-nunito text-md font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
                   >
-                    Create account
+                    Submit
+                  </button>
+                  <a
+                    className="block font-nunito text-base font-semibold leading-19 text-center text-primary_800_color justify-center pt-5 pb-10"
+                    onClick={() => navigate('/reset-password')}
+                  >
+                    Forgot password?
                   </a>
-                </p>
+                  <p className="font-nunito text-base font-normal leading-19 text-center">
+                    Don’t have an account yet?{' '}
+                    <a
+                      className="font-nunito text-base font-semibold leading-19 text-center text-primary_800_color hover:underline dark:text-primary-500"
+                      onClick={() => navigate('/register')}
+                    >
+                      Create account
+                    </a>
+                  </p>
+                </div>
               </form>
             </div>
           </div>
