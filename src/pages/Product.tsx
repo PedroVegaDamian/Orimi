d71793777f21d8f66890b4f9546892d645a70319
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-24 07:34:48 +1200

    feat: add Product component

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,3 +1,22 @@
-export const ProductsPage = () => {
-  return <div>ProductsPage</div>
+export const ProductPage = () => {
+  return (
+    <>
+     
+      <div className="bg-bg_color">
+        <div className="h-[20vh] w-full">
+          {/* <img src={HeaderImage} alt="header" /> */}
+        </div>
+
+        <div className="m-40">
+          <h1 className="font-nunito text-22 font-bold text-center text-primary_800_color text-2xl">
+            Best Sellers
+          </h1>
+          <div className="grid grid-cols-4 gap-4">
+          
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
 }
