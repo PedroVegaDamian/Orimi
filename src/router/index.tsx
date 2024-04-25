1d48f2db2e499743e833166fa5bb5b90072103cf
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-25 16:01:19 +1200

    feat:almost done

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -19,7 +19,7 @@ export const router = createBrowserRouter([
         element: <HomePage />
       },
       {
-        path: 'product',
+        path: 'product/:slug',
         element: <ProductPage />
       },
       {
