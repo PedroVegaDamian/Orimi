6f8e59589c7153d683bc7fbbd9b995f8522168df
Pedro Vega <pedrovegadamian@gmail.com>
2024-04-25 14:25:37 -0500

    fix: product model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 export interface Product {
   id: string
   description: string
-  image1: string
-  image2: string
-  image3: string
+  image: string
+  // Remplazar por:
+  // image1: string
+  // image2: string
+  // image3: string
   name: string
   price: string
   slug: string
