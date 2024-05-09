7cc195bbfe804a5f54488accbf078e2f86c04d52
Pedro Vega <pedrovegadamian@gmail.com>
2024-05-09 12:54:42 -0500

    fix: show name in navbar and resize footer

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
 
-export function Title ({children, ...props}:TitleProps){
-    return (
-        <h3 className="font-nunito font-bold text-22 text-center text-primary_800_color py-9" {...props}>
-            {children}
-        </h3>
-    )
-
+export function Title({ children, ...props }: TitleProps) {
+  return (
+    <h3
+      className="font-nunito font-bold text-22 text-center text-primary_800_color py-[36px]"
+      {...props}
+    >
+      {children}
+    </h3>
+  )
 }
