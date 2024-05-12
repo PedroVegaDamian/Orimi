01e194bd2d679249436efbdaa252051f63177afb
Rocio-Peralta <1rocioperalta@gmail.com>
2024-05-12 13:01:30 +1200

    fix: remove unnecessary href

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -107,7 +107,6 @@ export const ProductPage = () => {
                   </div>)}
                  
                   <button
-                    href="#"
                     className={`flex items-center justify-center bg-primary_color rounded-md bg-slate-900 mt-3 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary_500_color ${
                       isClicked ? 'cursor-not-allowed bg-primary_500_color' : ''
                     }`}
