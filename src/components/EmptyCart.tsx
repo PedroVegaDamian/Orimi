8148d51c1600fcf58f5936430dc563d18d48aab7
Rocio-Peralta <1rocioperalta@gmail.com>
2024-05-14 15:54:51 +1200

    ui: fiished empty cart

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,17 +1,31 @@
-import { Button } from './ui'
 import { useNavigate } from 'react-router-dom'
+import SadShoppingBag from '@/assets/icons/icon_sad_bag.svg'
 
 export const EmptyCart = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col items-center mt-20 justify-toph-screen">
-        <h1 className="text-3xl font-bold text-center text-primary_800_color">
+    <div className="">
+      <div className="grid grid-col">
+        <h1 className="mt-28 text-[48px] font-medium text-center text-primary_800_color">
           Your cart is empty
         </h1>
-        <div className="flex mt-10">
-          <Button onClick={() => navigate('/')}>Go back to home</Button>
+        <div className="flex flex-row justify-start h-screen items-start">
+          <div className="flex pt-10 pr-44 items-start justify-end w-1/2">
+            <img src={SadShoppingBag} className=""></img>
+          </div>
+          <div className="flex flex-col items-start pl-44 justify-start pt-40 w-1/2 h-full">
+            <p className="flex flex-row text-xl"> Add items to get started</p>
+
+            <div className="gap-3 mt-8">
+              <button
+                onClick={() => navigate('/')}
+                className="w-50 bg-primary_color font-nunito text-lg font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+              >
+                Continue Shopping
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
