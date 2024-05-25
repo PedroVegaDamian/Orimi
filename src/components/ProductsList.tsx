2b9cb9c81727f3547e0540fd0a0575e79b9bc7a2
Rocio-Peralta <1rocioperalta@gmail.com>
2024-05-25 17:24:34 +1200

    feat: implementing  lazy load img

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react'
 import { getProducts } from '@/services/getProducts'
 import { Product } from '@/models'
 import { Link } from 'react-router-dom'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import PlaceHolder from '@/assets/icons/placeholder-loading.svg'
 
-
- const ProductItem = () => {
+const ProductItem = () => {
   const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
@@ -21,13 +23,20 @@ import { Link } from 'react-router-dom'
         <div key={product.slug}>
           <Link to={`/product/${product.slug}`}>
             <div className="flex flex-col bg-white">
-              <div className="h-60 ">
-                <img
-                  className="rounded-lg aspect-square"
-                  src={product.image1}
-                  alt={product.name}
-                />
-              </div>
+          
+                <div className="h-60 ">
+                  <LazyLoadImage
+                    className="rounded-lg aspect-square"
+                    effect="blur"
+                    alt={product.name}
+                    src={product.image1}  
+                    // height={2500}
+                    // width={250}
+                    placeholderSrc={PlaceHolder} 
+            
+                  />
+                </div>
+          
               <div className="mt-4 px-5 pb-5">
                 <h5 className=" items-center font-nunito text-20 font-bold text-center text-primary_800_color text-2xl">
                   {product.name}
