3b81ae8df37981cf2671fe8196031f07d288a13c
Apaulav <paulaville@gmail.com>
2024-05-29 11:14:04 +0200

    feat: add about page

---
+++ src/components/AboutPage/DefaultText.tsx
@@ -0,0 +1,25 @@
+const DefaultText = () => (
+    <>
+        <h1 className="mb-20 font-nunito text-22 font-bold text-center text-primary_800_color text-2xl">
+            About us
+        </h1>
+        <p className="pb-5">
+            Hello and welcome to our origami shop!
+            We are a passionate team formed by Carolina, Pedro, Rocío, Silvana, and Paula, united by our shared love for the art of origami. We are dedicated to sharing the beauty and creativity of this ancient Japanese technique through our carefully curated selection of handmade origamis.
+        </p>
+        <p className="pb-5">
+            Each piece in our collection is unique, created with precision and dedication to capture the essence and delicacy of this millennia-old art. From paper cranes to lotus flowers, our goal is to offer you a unique experience as you explore the world of origami.
+        </p>
+        <p className="pb-5">
+            In our store, you will find a wide variety of origamis, from classics to modern and custom designs. Whether you're looking for a special gift or wishing to add a touch of elegance to your home, we are here to help you find the perfect piece.
+        </p>
+        <p className="pb-5">
+            We take pride in offering high-quality products, exceptional customer service, and a welcoming environment where you can explore and discover the magic of origami. Thank you for supporting our dream and for being part of our community!
+        </p>
+        <p className="pb-5">
+            Welcome to our world of folds and creativity!
+        </p>
+    </>
+);
+
+export default DefaultText;
---
+++ src/components/AboutPage/PedroText.tsx
@@ -0,0 +1,18 @@
+const PedroText = () => (
+    <>
+        <h1 className="mb-20 font-nunito text-22 font-bold text-center text-primary_800_color text-2xl">
+            Pedro Vega Damian - Frontend Developer
+        </h1>
+        <p className="pb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at convallis nunc. Duis nec nisi nunc. Nullam venenatis dignissim libero, id vehicula tortor vestibulum non. Vivamus elementum lorem sed lectus volutpat, ac fringilla tortor aliquet. Curabitur blandit malesuada turpis, nec tincidunt est gravida in. Aenean ut turpis ac sapien hendrerit auctor. Nullam imperdiet leo at massa egestas, a dignissim libero fermentum. Nulla facilisi. Praesent in metus ultricies, consequat ligula vel, elementum nunc.
+        </p>
+        <p className="pb-5">
+            Proin fringilla varius libero, ac efficitur lorem lacinia non. Suspendisse potenti. Ut cursus eros id est ultricies fermentum. Nam in fringilla magna. Nulla vitae justo risus. Phasellus sodales diam quis velit cursus, ut convallis metus blandit. Curabitur consequat lectus at lacus vehicula, nec tincidunt odio tristique. Integer ut leo purus. Suspendisse potenti. Sed eu vestibulum ligula. Cras quis dolor ac quam convallis venenatis. Sed in justo sit amet velit laoreet suscipit.
+        </p>
+        <p className="pb-5">
+            Aliquam erat volutpat. Phasellus faucibus semper sapien, non tincidunt orci finibus a. Pellentesque ac malesuada metus. Nullam consequat, purus sit amet blandit auctor, ex risus cursus nunc, eget malesuada justo nulla a metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse potenti. Integer gravida, eros eget sodales lacinia, neque ex faucibus ligula, in pharetra odio elit sit amet dui. Nulla at egestas dui, et pharetra turpis. Suspendisse potenti. Curabitur dictum cursus vehicula. Suspendisse scelerisque diam turpis, eget congue urna luctus in.
+        </p>
+    </>
+);
+
+export default PedroText;
