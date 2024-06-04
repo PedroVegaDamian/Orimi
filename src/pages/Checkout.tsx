3dcce77b0495bb65cff3cd5ed1b056945867c94e
Rocio-Peralta <1rocioperalta@gmail.com>
2024-06-04 19:33:01 +1200

    feat:checkout working

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,8 +1,39 @@
-const CheckoutPage = () => {
+import { loadStripe } from '@stripe/stripe-js'
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js'
+import { useCallback } from 'react'
+import { useCartStore } from '@/store/cartStore'
 
-  return(
-    <h1> Checkout page</h1>
+const stripePromise = loadStripe(import.meta.env.VITE_API_KEY_PUBLIC_STRIPE)
+
+const CheckoutPage = () => {
+  const cart = useCartStore(state => state.cart)
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const res = await fetch(
+      'https://orimi-checkout.orimi.workers.dev/create-checkout-session',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cart)
+      }
+    )
+    const data = await res.json()
+    return data.clientSecret
+  }, [])
+  const options = { fetchClientSecret }
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
   )
 }
 
-export default CheckoutPage;
+export default CheckoutPage
