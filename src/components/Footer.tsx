e5cdb3d027525c4cc720d02d2f194e62fa5b88b4
Carolhs92 <carolhersant4@gmail.com>
2024-06-06 14:43:44 +0200

    ui: responsive Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,46 +2,49 @@ import IconHeart from '@/assets/icons/icon_heart.svg'
 
 export const Footer = () => {
   return (
-    <footer className="bg-white_color py-6 flex font-inter text-[24px] justify-center gap-4 sticky bottom-0">
-      <div className="flex justify-center">
-        <img className="w-[31px] mr-2" src={IconHeart} alt="Heart Icon" />
-        <p>Made by:</p>
+    <footer className="bg-white_color py-6 text-[24px] lg:sticky fixed inset-x-0 bottom-0">
+      <div className="flex justify-center lg:order-none order-1 items-baseline">
+        <img className="md:w-[31px] lg:w-[31px] w-[20px] h-[20px] mr-2" src={IconHeart} alt="Heart Icon" />
+        <p className='lg:text-[24px] text-[16px]'>Made by:</p>
       </div>
-      <a
-        target="_blank"
-        href="https://github.com/Carolhs92"
-        className="underline  text-primary_tono1_color"
-      >
-        Carolina
-      </a>
-      <a
-        target="_blank"
-        href="https://github.com/PedroVegaDamian"
-        className="underline text-primary_tono2_color"
-      >
-        Pedro
-      </a>
-      <a
-        target="_blank"
-        href="https://github.com/rocio-peralta"
-        className="underline text-primary_tono3_color"
-      >
-        Rocío
-      </a>
-      <a
-        target="_blank"
-        href="https://github.com/SLouQA"
-        className="underline text-primary_tono4_color"
-      >
-        Silvana
-      </a>
-      <a
-        target="_blank"
-        href="https://github.com/Apaulav"
-        className="underline text-primary_tono5_color"
-      >
-        Paula
-      </a>
+      <div className='flex justify-center gap-4 order-2'>
+        <a
+          target="_blank"
+          href="https://github.com/Carolhs92"
+          className="underline text-primary_tono1_color lg:text-[24px] text-[16px]"
+        >
+          Carolina
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/PedroVegaDamian"
+          className="underline text-primary_tono2_color lg:text-[24px] text-[16px]"
+        >
+          Pedro
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/rocio-peralta"
+          className="underline text-primary_tono3_color lg:text-[24px] text-[16px]"
+        >
+          Rocío
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/SLouQA"
+          className="underline text-primary_tono4_color lg:text-[24px] text-[16px]"
+        >
+          Silvana
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/Apaulav"
+          className="underline text-primary_tono5_color lg:text-[24px] text-[16px]"
+        >
+          Paula
+        </a>
+      </div>
+      
     </footer>
   )
 }
