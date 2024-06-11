3da0dbf95b478c6d1c87629e3aa6cc7d1debb51e
Pedro Vega <pedrovegadamian@gmail.com>
2024-06-10 19:23:42 -0500

    refactor: add limit to useFetchProduct

--- src/hooks/useFetchProducts.ts
+++ src/hooks/useFetchProducts.ts
@@ -1,27 +1,21 @@
-import { useState, useEffect } from 'react';
-import { db } from '@/firebase/index';
-import { collection, getDocs } from 'firebase/firestore';
-import { Product as ProductType } from '@/models';
+import { useState, useEffect } from 'react'
+import { getProducts } from '@/services/getProducts'
+import { Product as ProductType, OptionsProducts } from '@/models'
 
-export const useFetchProducts = () => {
-    const [products, setProducts] = useState<ProductType[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
+export const useFetchProducts = (options?: OptionsProducts) => {
+  const [products, setProducts] = useState<ProductType[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-        setLoading(true);
-        const productsCollection = collection(db, 'products');
-        const productsSnapshot = await getDocs(productsCollection);
-        const productsData = productsSnapshot.docs.map(doc => {
-            const data = doc.data() as Omit<ProductType, 'id'>;
-            return { id: doc.id, ...data };
-        });
-        setProducts(productsData);
-        setLoading(false);
-        };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true)
+      const productsData = await getProducts(options)
+      setProducts(productsData)
+      setLoading(false)
+    }
 
-        fetchProducts();
-    }, []);
+    fetchProducts()
+  }, [options])
 
-    return { products, loading };
-};
+  return { products, loading }
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,17 +1,21 @@
 export interface Product {
-  id: string;
-  name: string;
-  category: string;
-  description: string;
-  image1: string;
-  image2: string;
-  image3: string;
-  price: number;
-  slug: string;
-  stock: number;
-  quantity: number;
-  subtotal: number;
-  isStock: boolean;
+  id: string
+  name: string
+  category: string
+  description: string
+  image1: string
+  image2: string
+  image3: string
+  price: number
+  slug: string
+  stock: number
+  quantity: number
+  subtotal: number
+  isStock: boolean
+}
+
+export interface OptionsProducts {
+  limit?: number
 }
 
 export interface CartState {
@@ -25,7 +29,6 @@ export interface CartState {
   isStock: (id: string | undefined) => void
 }
 
-
 export enum Categories {
   All = 'All',
   Birds = 'birds',
@@ -33,5 +36,5 @@ export enum Categories {
   Amphibians = 'amphibians',
   Fish = 'fish',
   Insects = 'insects',
-  Reptiles = 'reptiles',
+  Reptiles = 'reptiles'
 }
