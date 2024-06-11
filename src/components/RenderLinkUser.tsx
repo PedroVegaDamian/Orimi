605bfecb716c19a24c7d95beeb854f6d8cb06956
Carolhs92 <carolhersant4@gmail.com>
2024-06-11 16:27:50 +0200

    fix: Navbar truncate

--- src/components/RenderLinkUser.tsx
+++ src/components/RenderLinkUser.tsx
@@ -16,8 +16,10 @@ export const RenderLinkUser = ({ className = '', onClick }: RenderLinkUserProps)
 
     if (user) {
         return (
-            <Link to="/profile" className={`block bg-primary_color py-3 px-4 rounded-xl max-w-40 truncate ${className}`} onClick={onClick}>
-                Hi {user.firstName} {user.lastName}!
+            <Link to="/profile" className={`block bg-primary_color py-3 px-4 rounded-xl max-w-[10rem] ${className} overflow-hidden text-ellipsis whitespace-nowrap`}>
+                <span className="block max-w-full overflow-hidden text-ellipsis whitespace-nowrap">
+                    Hi {user.firstName} {user.lastName}!
+                </span>
             </Link>
         );
     }
