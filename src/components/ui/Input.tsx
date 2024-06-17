fb888a56eea8163911eab763b969c1838a9a7caf
Carolhs92 <carolhersant4@gmail.com>
2024-06-17 16:39:53 +0200

    feat: add Contact

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export function Input({ className, ...props }: InputProps) {
-  const baseStyles =
-    'border-1 border-grey_color rounded-10 px-[17px] md:w-[475px] h-[40px]'
-  const combinedClasses = `${baseStyles} ${className || ''}`
+export const Input: React.FC<InputProps> = ({ className, ...props }) => {
+  const baseStyles = 'border-1 border-grey_color rounded-10 px-[17px] md:w-[475px] h-[40px]';
+  const combinedClasses = `${baseStyles} ${className || ''}`;
 
-  return <input {...props} className={combinedClasses} />
-}
+  return <input {...props} className={combinedClasses} />;
+};
