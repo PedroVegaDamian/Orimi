58616efaec65cf02be004e71ffbbd32d903c1c20
Rocio-Peralta <1rocioperalta@gmail.com>
2024-06-19 07:39:38 +1200

    feat: send user to the backend

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -7,10 +7,12 @@ import { Increment } from '@/components/Increment'
 import { Decrement } from '@/components/Decrement'
 import { useNavigate } from 'react-router-dom'
 
- const CartPage = () => {
+const CartPage = () => {
   const { total, cart, removeProduct, multiply, totalSum } = useCartStore()
-  const {user} = useUserStore()
- 
+  const { user } = useUserStore()
+
+  console.log('USER :' + user?.email)
+
   const navigate = useNavigate()
 
   const handleRemove = (slug: string | undefined) => {
@@ -20,13 +22,13 @@ import { useNavigate } from 'react-router-dom'
   }
   const handleClick = () => {
     if (user) {
-     navigate('/checkout')
+      navigate('/checkout')
     } else {
-     navigate('/login')
+      navigate('/login')
     }
   }
   return (
-    <div className='bg-white_color'>
+    <div className="bg-white_color">
       {cart.length === 0 ? (
         <EmptyCart />
       ) : (
@@ -41,7 +43,9 @@ import { useNavigate } from 'react-router-dom'
                 <span className="pl-20 w-full max-w-[260px] text-center">
                   Quantity
                 </span>
-                <span className="w-full max-w-[200px] pl-8 text-center">Total</span>
+                <span className="w-full max-w-[200px] pl-8 text-center">
+                  Total
+                </span>
               </p>
             </div>
             {cart.map(product => (
@@ -113,8 +117,9 @@ import { useNavigate } from 'react-router-dom'
               </div>
             </div>
             <div className="flex items-end flex-col justify-center gap-3 mt-8">
-              <button className="w-50 bg-primary_color font-nunito text-lg font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-              onClick={handleClick}
+              <button
+                className="w-50 bg-primary_color font-nunito text-lg font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+                onClick={handleClick}
               >
                 Continue to Payment
               </button>
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -5,11 +5,13 @@ import {
 } from '@stripe/react-stripe-js'
 import { useCallback } from 'react'
 import { useCartStore } from '@/store/cartStore'
+import { useUserStore } from '@/store/userStore'
 
 const stripePromise = loadStripe(import.meta.env.VITE_API_KEY_PUBLIC_STRIPE)
 
 const CheckoutPage = () => {
   const cart = useCartStore(state => state.cart)
+  const user = useUserStore(state => state.user)
   const fetchClientSecret = useCallback(async () => {
     // Create a Checkout Session
     const res = await fetch(
@@ -22,7 +24,7 @@ const CheckoutPage = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(cart)
+        body: JSON.stringify({cart, user})
       }
     )
     const data = await res.json()
