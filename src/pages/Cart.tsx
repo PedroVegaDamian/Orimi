8a08219018057be7e7803c098e036d6c9ba39f90
Pedro Vega <pedrovegadamian@gmail.com>
2024-06-18 13:52:18 -0500

    refactor: add redirect login and fix checkout

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -7,10 +7,10 @@ import { Increment } from '@/components/Increment'
 import { Decrement } from '@/components/Decrement'
 import { useNavigate } from 'react-router-dom'
 
- const CartPage = () => {
+const CartPage = () => {
   const { total, cart, removeProduct, multiply, totalSum } = useCartStore()
-  const {user} = useUserStore()
- 
+  const { user } = useUserStore()
+
   const navigate = useNavigate()
 
   const handleRemove = (slug: string | undefined) => {
@@ -20,13 +20,13 @@ import { useNavigate } from 'react-router-dom'
   }
   const handleClick = () => {
     if (user) {
-     navigate('/checkout')
+      navigate('/checkout')
     } else {
-     navigate('/login')
+      navigate('/login?redirect=/checkout')
     }
   }
   return (
-    <div className='bg-white_color'>
+    <div className="bg-white_color">
       {cart.length === 0 ? (
         <EmptyCart />
       ) : (
@@ -41,7 +41,9 @@ import { useNavigate } from 'react-router-dom'
                 <span className="pl-20 w-full max-w-[260px] text-center">
                   Quantity
                 </span>
-                <span className="w-full max-w-[200px] pl-8 text-center">Total</span>
+                <span className="w-full max-w-[200px] pl-8 text-center">
+                  Total
+                </span>
               </p>
             </div>
             {cart.map(product => (
@@ -113,8 +115,9 @@ import { useNavigate } from 'react-router-dom'
               </div>
             </div>
             <div className="flex items-end flex-col justify-center gap-3 mt-8">
-              <button className="w-50 bg-primary_color font-nunito text-lg font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-              onClick={handleClick}
+              <button
+                className="w-50 bg-primary_color font-nunito text-lg font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+                onClick={handleClick}
               >
                 Continue to Payment
               </button>
