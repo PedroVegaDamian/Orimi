1a477645c836d6498f5e66f3fe84a403f8f14cea
Carolhs92 <carolhersant4@gmail.com>
2024-06-26 20:45:55 +0200

    Fix: Error User

--- src/pages/PaymentSuccesfull.tsx
+++ src/pages/PaymentSuccesfull.tsx
@@ -1,15 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import IconCheck from '@/assets/icons/icon_check.svg';
-import { useEffect } from 'react';
-import { useCartStore } from '@/store/cartStore';
-import addOrder from '@/services/addOrder';
-import { useUserStore } from '@/store/userStore';
-import { Title, Button } from '@/components/ui';
+import { useNavigate } from 'react-router-dom'
+import IconCheck from '@/assets/icons/icon_check.svg'
+import { useEffect } from 'react'
+import { useCartStore } from '@/store/cartStore'
+import addOrder from '@/services/addOrder'
 
-const PaymentSuccessfulPage = () => {
-  const { cart, resetCart } = useCartStore();
-  const navigate = useNavigate();
-  const { user } = useUserStore();
+const PaymentSuccesfullPage = () => {
+  const { cart, resetCart } = useCartStore()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const queryString = window.location.search
@@ -25,23 +22,32 @@ const PaymentSuccessfulPage = () => {
   }, [])
 
   return (
-    <div className="flex justify-center items-center w-full mt-10">
-      <div className="p-4 w-full max-w-md h-full">
-        <div className="p-4 text-center bg-white_color rounded-lg shadow">
-          <div className="w-12 h-12 rounded-full bg-green_color p-2 flex items-center justify-center mx-auto mb-3.5">
-            <img src={IconCheck} className="h-56" alt="Payment Successful" />
-          </div>
-          <Title>Payment Done!</Title>
-          <p className="text-lg">
-            Thank you for completing your secure online payment.
-          </p>
-          <div className="gap-3 mt-8">
-            <Button onClick={() => navigate('/profile/orders')}>See your order</Button>
+    <>
+      <div className=" flex justify-center items-center w-full mt-10">
+        <div className="p-4 w-full max-w-md h-full">
+          <div className="p-4 text-center  bg-white_color rounded-lg shadow ">
+            <div className="w-12 h-12 rounded-full bg-green_color p-2 flex items-center justify-center mx-auto mb-3.5">
+              <img src={IconCheck} className="h-56"></img>
+            </div>
+            <h1 className="text-2xl font-medium text-center text-primary_800_color">
+              Payment Done!
+            </h1>
+            <p className="text-lg">
+              Thank you for completing your secure online payment.
+            </p>
+            <div className="gap-3 mt-8">
+              <button
+                onClick={() => navigate('/')}
+                className="w-30 bg-primary_color font-nunito text-base font-semibold leading-22 text-center bg-purple-400 hover:bg-primary-700 focus:bg-primary_500_color  rounded-lg px-5 py-2.5  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+              >
+                Continue
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default PaymentSuccessfulPage;
+export default PaymentSuccesfullPage
