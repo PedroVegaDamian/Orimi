bb7c74d2d60daa30d60aaa306723f663fc20f1ac
Rocio-Peralta <1rocioperalta@gmail.com>
2024-07-09 20:42:22 +1200

    feature: working on pass validation

--- src/pages/ConfirmChangePassword.tsx
+++ src/pages/ConfirmChangePassword.tsx
@@ -5,6 +5,12 @@ import {
   confirmPasswordReset,
 } from 'firebase/auth'
 import { useState } from 'react'
+import { ErrorMessage, Input, Label,  } from '@/components/ui'
+import { errorMessages, CustomErrorCodes } from '@/utils/errorCodeMessages'
+import {
+  passwordRegex,
+} from '@/utils/validationsRegex'
+
 
 const ConfirmChangePasswordPage = () => {
   const location = useLocation()
@@ -13,13 +19,54 @@ const ConfirmChangePasswordPage = () => {
   const actionCode = searchParams.get('oobCode')
   const auth = getAuth()
   const [newPassword, setNewPassword] = useState('')
+  const [passwordError, setPasswordError] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>('')
   const navigate = useNavigate()
+ 
+ 
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const newErrors: { [key: string]: string } = {}
+    let isValid = true
+
+    setPasswordError('')
+    setConfirmPasswordError('')
+
+    // Validación de la contraseña
+    if (newPassword) {
+      newErrors.passwordError = errorMessages[CustomErrorCodes.REQUIRED_FIELD]
+      isValid = false
+    } else if (!passwordRegex.test(newPassword)) {
+      newErrors.passwordError = errorMessages[CustomErrorCodes.WEAK_PASSWORD]
+      isValid = false
+    }
+
+    // Validación de confirmación de contraseña
+    if (!confirmPassword) {
+      newErrors.confirmPasswordError =
+        errorMessages[CustomErrorCodes.REQUIRED_FIELD]
+      isValid = false
+    } else if (newPassword !== confirmPassword) {
+      newErrors.confirmPasswordError =
+        errorMessages[CustomErrorCodes.INVALID_CONFIRM_PASSWORD]
+      isValid = false
+    }
+    setPasswordError(newErrors.passwordError || '')
+    setConfirmPasswordError(newErrors.confirmPasswordError || '')
+
+    if (!isValid) {
+      return
+    }
+  }
 
   const handleClick = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (mode === 'resetPassword' && actionCode) {
       try {
         await verifyPasswordResetCode(auth, actionCode)
+        handleRegister(e)
         await confirmPasswordReset(auth, actionCode, newPassword)
         alert('Password has been reset successfully.')
         navigate('/login')
@@ -42,17 +89,61 @@ const ConfirmChangePasswordPage = () => {
               Reset password
             </h1>
             <form onSubmit={handleClick}>
-              <input
+              {/* <input
                 name="New password"
                 className="border border-solid border-grey_color text-gray-900 rounded-lg focus:ring-red-500 focus:border-grey_800_color block w-full p-2.5"
                 placeholder="*********"
                 type="password"
                 onChange={e => setNewPassword(e.target.value)}
               ></input>
-              <p className="text-sm pt-5">
+              <p className="text-sm pt-5 text-grey_500_color">
                 The password must have: at least 6 characters, one uppercase
                 letter, one lowercase letter and one number.
               </p>
+              <Label htmlFor="confirmPassword">
+              Confirm Password<span className="text-red_color">*</span>
+            </Label>
+            <div style={{ height: '20px' }}>
+              <ErrorMessage message={passwordError} />
+            </div> */}
+    <div className="flex flex-col flex-nowrap justify-center content-center max-w-[450px]">
+            <Label htmlFor="password">
+              Password<span className="text-red_color">*</span>
+            </Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              value={newPassword}
+              onChange={e => setNewPassword(e.target.value)}
+            />
+            <small className="text-grey_500_color">
+              The password must have: at least 6 characters, one uppercase
+              letter, one lowercase letter and one number.
+            </small>
+            <div style={{ height: '20px' }}>
+              <ErrorMessage message={passwordError} />
+            </div>
+          </div>
+
+          <div className="flex flex-col flex-nowrap justify-center content-center max-w-[450px]">
+            <Label htmlFor="confirmPassword">
+              Confirm Password<span className="text-red_color">*</span>
+            </Label>
+            <Input
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+            />
+            <div style={{ height: '20px' }}>
+              <ErrorMessage message={confirmPasswordError} />
+            </div>
+          </div>
+
               <div className="gap-3 mt-8">
                 <button
                   type="submit"
