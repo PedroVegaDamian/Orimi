9e5bf669e89a23534cf102e0b493b95758db4cf4
Rocio-Peralta <1rocioperalta@gmail.com>
2024-07-12 07:12:25 +1200

    bug: changed about me text

--- src/components/AboutPage/DefaultText.tsx
+++ src/components/AboutPage/DefaultText.tsx
@@ -1,25 +1,36 @@
-import { Title } from "@/components/ui/Title";
+import { Title } from '@/components/ui/Title'
 
 const DefaultText = () => (
-    <div className="max-w-[90%] mx-auto mb-[100px] md:ml-9 lg:ml-[100px]">
-        <Title> About us </Title>
-        <p className="pb-5">
-            Hello and welcome to our origami shop!
-            We are a passionate team formed by Carolina, Pedro, Rocío, Silvana, and Paula, united by our shared love for the art of origami. We are dedicated to sharing the beauty and creativity of this ancient Japanese technique through our carefully curated selection of handmade origamis.
-        </p>
-        <p className="pb-5">
-            Each piece in our collection is unique, created with precision and dedication to capture the essence and delicacy of this millennia-old art. From paper cranes to lotus flowers, our goal is to offer you a unique experience as you explore the world of origami.
-        </p>
-        <p className="pb-5">
-            In our store, you will find a wide variety of origamis, from classics to modern and custom designs. Whether you're looking for a special gift or wishing to add a touch of elegance to your home, we are here to help you find the perfect piece.
-        </p>
-        <p className="pb-5">
-            We take pride in offering high-quality products, exceptional customer service, and a welcoming environment where you can explore and discover the magic of origami. Thank you for supporting our dream and for being part of our community!
-        </p>
-        <p className="pb-5">
-            Welcome to our world of folds and creativity!
-        </p>
-    </div>
-);
+  <div className="max-w-[90%] mx-auto mb-[100px] md:ml-9 lg:ml-[100px]">
+    <Title> About us </Title>
+    <p className="pb-5">Welcome to Orimi!</p>
+    <p className="pb-5">
+      Orimi was born out of our desire to challenge ourselves and create a
+      project we could be proud of—and we nailed it! We are a team of three
+      developers and one QA specialist who came together through the magic of
+      Discord and YouTube. Despite being strangers initially, we successfully
+      organized and managed our time to put together this amazing and fully
+      functional project.
+    </p>
+    <p className="pb-5">
+      Our journey began with planning on Figma and collaborating through
+      Discord, seamlessly integrating Jira and GitHub into our workflow. Working
+      across three different continents—South America, Europe, and Oceania—our
+      work was asynchronous, with daily meetings four days a week at times
+      convenient for everyone.
+    </p>
+    <p className="pb-5">
+      We invite you to explore our site: create a profile, update your address,
+      check your order history, and buy as many origamis as you like. Don’t
+      forget to check out our payment method implementation and maybe even reset
+      your password for fun! All functionalities are working—have a play around.
+    </p>
+    <p className="pb-5">
+      Feel free to reach out or make any suggestions—we’re here to listen and
+      improve. We hope you enjoy our site as much as we enjoyed creating it.
+    </p>c
+    <p className="pb-5">Thank you for visiting Orimi!</p>
+  </div>
+)
 
-export default DefaultText;
+export default DefaultText
